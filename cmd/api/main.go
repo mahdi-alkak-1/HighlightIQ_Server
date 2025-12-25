5dace0086c4570b0ccf90e3cc9c77a60e7550395
mahdi-alkak1-1 <mahdialkak1@gmail.com>
2025-12-25 22:16:10 +0200

    Router created

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,19 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"highlightiq-server/internal/http/router"
 )
 
 func main() {
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte("ok"))
-	})
+	r := router.New()
 
 	addr := ":8080"
 	log.Println("API listening on", addr)
-	if err := http.ListenAndServe(addr, mux); err != nil {
+	if err := http.ListenAndServe(addr, r); err != nil {
 		log.Fatal(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module highlightiq-server
 
 go 1.25.5
+
+require github.com/go-chi/chi/v5 v5.2.3 // indirect
